e1e8078074ede302d470cce24b99315b90fa4f99
Almog Oz <almogoz811@gmail.com>
2026-01-24 19:11:17 +0200

    Already implemented

--- src/main/java/org/ditto/datapulse/DataPulseConsoleApplication.java
+++ src/main/java/org/ditto/datapulse/DataPulseConsoleApplication.java
@@ -1,17 +1,23 @@
 package org.ditto.datapulse;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
+@RequiredArgsConstructor
 public class DataPulseConsoleApplication implements CommandLineRunner {
+    private final DataPulseExecutor executor;
+
     public static void main(String[] args) {
         SpringApplication.run(DataPulseConsoleApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        this.executor.run();
     }
 }
--- src/main/java/org/ditto/datapulse/configuration/execution/ExecutionConfiguration.java
+++ src/main/java/org/ditto/datapulse/configuration/execution/ExecutionConfiguration.java
@@ -2,6 +2,7 @@ package org.ditto.datapulse.configuration.execution;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.ditto.datapulse.models.execution.strategy.ExecutionStrategyConfiguration;
 import org.ditto.datapulse.models.execution.trigger.TriggerConfiguration;
 import org.ditto.datapulse.utils.JacksonPolymorphicUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -14,14 +15,25 @@ import java.util.Map;
 @ConfigurationProperties("data-pulse.execution")
 public class ExecutionConfiguration {
     private TriggerConfiguration trigger;
+    private ExecutionStrategyConfiguration strategy;
 
     @JsonSetter
     public void setTrigger(Map<String, Object> trigger) throws JsonProcessingException {
         this.trigger = JacksonPolymorphicUtils.convertToPolymorphicType(trigger, TriggerConfiguration.class);
     }
 
+    @JsonSetter
+    public void setStrategy(Map<String, Object> strategy) throws JsonProcessingException {
+        this.strategy = JacksonPolymorphicUtils.convertToPolymorphicType(strategy, ExecutionStrategyConfiguration.class);
+    }
+
     @Bean
     public TriggerConfiguration triggerConfiguration() {
         return this.trigger;
+    }
+
+    @Bean
+    public ExecutionStrategyConfiguration strategyConfiguration() {
+        return this.strategy;
     }
 }
